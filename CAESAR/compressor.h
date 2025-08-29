693dd053674da29009a610b930ee40dd638562f2
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-08-29 10:28:23 -0400

    looks ok for now

--- CAESAR/compressor.h
+++ CAESAR/compressor.h
@@ -5,6 +5,23 @@
 #include <torch/torch.h>
 #include <torch/script.h>
 #include  <map>
+#include <utility>
+
+
+struct CompressionResult {
+  // A bit of a guess for right now 
+    std::vector<float> latent;
+    std::vector<uint8_t> postProcess;
+    std::map<std::string, int> meta_data;
+    std::vector<int> shape;
+    std::vector<int> padding;
+    std::vector<int> filteredBlocks;
+
+};
+
+
+// for compression method
+using CompressReturn = std::pair<CompressionResult, int>;
 
 class Compressor {
 public:
@@ -17,10 +34,31 @@ public:
     
    std::map<std::string, torch::Tensor> removeModulePrefix(const std::map<std::string, torch::Tensor>& stateDict);
 
+   // what the hell is a dataloader look into it more this also needs to be saved
+ //  CompressReturn compress(DataLoader& data)
+
+
+
+
+
+
 private:
 
    void loadModels();
+ 
+   //both here
+   // py def compress(self, dataloader, eb = 1e-3):
+   
+
+   // CAESAR V methods ------------------------
    void loadCaesarVCompressor();
+
+
+
+
+
+
+   // CAESAR D methods down ---------------------
    void loadCaesarDCompressor(); // later to add in 
 
     std::string pretrainedPath;
