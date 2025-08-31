d3f21441ce6392037c98bbbed19a3d39ad504387
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-08-31 18:44:44 -0400

    added BSConvU lybeary part seems to work i guess lots of magic math

---
+++ CAESAR/models/BCRN/blocks.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <torch/torch.h>         
+#include <torch/nn/functional.h>
+#include <vector>
+#include <map>
+#include <memory>              
+#include <iostream>
+#include "BSConvU.h"
+
+
+
+
+
