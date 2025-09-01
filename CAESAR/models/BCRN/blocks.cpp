faf81d6eef329b799d2a6ee5c1ef53304c04bdad
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-08-31 21:37:05 -0400

    starting to truely convert files and convert the models

--- CAESAR/models/BCRN/blocks.cpp
+++ CAESAR/models/BCRN/blocks.cpp
@@ -1,3 +1,43 @@
 #include "blocks.h"
+#include <algorithm>
+
+// rember to make a file to test this when done
+//NOTE:  assuming kernel size is 1d 
+torch::nn::Conv2d convLayer(int64_t inChannels, int64_t outChannels,
+        int64_t kernelSize, int64_t stride,
+        int64_t dilation, int64_t groups) {
+
+    int64_t padding = (kernelSize - 1) / 2 * dilation;
+  return torch::nn::Conv2d(torch::nn::Conv2dOptions(inChannels, outChannels, kernelSize)
+    .stride(stride)
+    .padding(padding)
+    .bias(true)
+    .dilation(dilation)
+    .groups(groups));  
+  
+}
 
 
+BSConvU bluePrintConvLayer(int64_t inChannels, int64_t outChannels,
+        int64_t kernelSize, int64_t stride, int64_t dilation) {
+
+    int64_t padding = (kernelSize - 1) / 2 * dilation;
+    
+    return BSConvU(inChannels, outChannels, kernelSize, stride, padding, 1);
+}
+
+
+torch::nn::AnyModule norm(const std::string& norm_type, int64_t nc) {
+    std::string norm_lower = norm_type;
+    std::transform(norm_lower.begin(), norm_lower.end(), norm_lower.begin(), ::tolower);
+    
+    if (norm_lower == "batch") {
+        auto layer = torch::nn::BatchNorm2d(torch::nn::BatchNorm2dOptions(nc).affine(true));
+        return torch::nn::AnyModule(layer);
+    } else if (norm_lower == "instance") {
+        auto layer = torch::nn::InstanceNorm2d(torch::nn::InstanceNorm2dOptions(nc).affine(false));
+        return torch::nn::AnyModule(layer);
+    } else {
+        throw std::runtime_error("normalization layer [" + norm_type + "] is not found");
+    }
+}
--- CAESAR/models/BCRN/blocks.h
+++ CAESAR/models/BCRN/blocks.h
@@ -1,14 +1,21 @@
 #pragma once
-
-#include <torch/torch.h>         
-#include <torch/nn/functional.h>
+#include <torch/torch.h>
+#include <tuple>
+#include <string>
 #include <vector>
 #include <map>
-#include <memory>              
 #include <iostream>
+#include <algorithm>
+#include <stdexcept>
 #include "BSConvU.h"
 
 
+torch::nn::Conv2d convLayer(int64_t inChannels, int64_t outChannels,
+        int64_t kernelSize, int64_t stride = 1,
+        int64_t dilation = 1, int64_t groups = 1);
 
+BSConvU bluePrintConvLayer(int64_t inChannels, int64_t outChannels,
+        int64_t kernelSize, int64_t stride = 1,
+        int64_t dilation = 1);
 
-
+torch::nn::AnyModule norm(const std::string& normType, int64_t nc);
