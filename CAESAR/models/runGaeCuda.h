76ceedab0b4debed514da967b12b391fb66eeb86
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-09-08 21:20:40 -0400

    cleaned it up now i can start to work

---
+++ CAESAR/models/runGaeCuda.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <chrono>
+#include <filesystem>
+#include <memory>
+#include <cstdlib>
+
+#include <nlohmann/json.hpp>
+
+#include <torch/torch.h>
+#include <torch/script.h>
+
+
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+
+#include <zstd.h> 
+
+
+#include <nvcomp/lz4.h>
+#include <nvcomp/cascaded.h>
+
