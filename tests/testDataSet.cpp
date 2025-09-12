d28d22ed91ebec315c45593fceeda1c400ef6645
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-09-11 22:58:34 -0400

    added testing files

---
+++ tests/testDataSet.cpp
@@ -0,0 +1,10 @@
+#include "../CAESAR/dataset/dataset.h"
+#include <iostream>
+
+
+int main() {
+    std::cout << "Starting to test dataset" << std::endl;
+    
+    std::cout<<"Done testing dataset"<<std::endl;
+    return 0;
+}
