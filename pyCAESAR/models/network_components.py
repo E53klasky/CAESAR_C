1350c9097f55db692257f6d36bfa5dd4023231d7
Ethan Klasky <klaskyethan@gmail.com>
2025-11-16 13:43:25 -0500

    removed compress ai

--- pyCAESAR/models/network_components.py
+++ pyCAESAR/models/network_components.py
@@ -6,12 +6,12 @@ import numpy as np
 from .utils import exists, LowerBound
 from einops import rearrange
 
-from compressai._CXX import pmf_to_quantized_cdf as _pmf_to_quantized_cdf
+#from compressai._CXX import pmf_to_quantized_cdf as _pmf_to_quantized_cdf
 
 
 
 def pmf_to_quantized_cdf(pmf, precision = 16):
-    cdf = _pmf_to_quantized_cdf(pmf.tolist(), precision)
+  #  cdf = _pmf_to_quantized_cdf(pmf.tolist(), precision)
     cdf = torch.IntTensor(cdf)
     return cdf
 
